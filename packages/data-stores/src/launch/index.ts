1e29d42241ac7ad4e219b4b4dcab0fe5adfcc5e4
Matthew Reishus <mreishus@users.noreply.github.com>
2021-01-18 12:16:57 -0600

    Only launchImmediately for anchor gutenboarding

--- packages/data-stores/src/launch/index.ts
+++ packages/data-stores/src/launch/index.ts
@@ -35,6 +35,7 @@ export function register(): typeof STORE_KEY {
 				'planProductId',
 				'confirmedDomainSelection',
 				'isExperimental',
+				'isAnchorFm',
 				'isSiteTitleStepVisible',
 				'shouldDisplaySuccessView',
 			],
